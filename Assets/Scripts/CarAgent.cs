f30c79456272d783b2aec928d2b84572ac396a68
arshavyna <anasbayu03@gmail.com>
2024-10-13 15:00:38 +0700

    first commit

---
+++ Assets/Scripts/CarAgent.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class CarAgent : Agent
+{
+    
+    public override void Initialize(){
+
+    }
+
+    public override void OnEpisodeBegin(){
+    
+    }
+
+    public override void CollectObservations(VectorSensor sensor){
+    
+    }
+
+    // public override void OnActionReceived(float[] vectorAction){
+    
+    // }
+
+    // public override void Heuristic(float[] actionsOut){
+    
+    // }
+
+}
